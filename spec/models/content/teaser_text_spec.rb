f843aeed220a6516607948d5d09eac4048357479
Kieran Hayes <kieran1977@gmail.com>
2011-08-11 17:42:17 +0200

    added content model specs

---
+++ spec/models/content/teaser_text_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Content::TeaserText do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/models/content/welcome_page_teaser_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Content::WelcomePageTeaser do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
